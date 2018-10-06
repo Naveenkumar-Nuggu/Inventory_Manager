1606cd58a37b7dd42f9c83b23ad13dcd7c657065
NaveenKumar Nuggu <S531373@nwmissouri.edu>
2018-10-05 19:43:50 -0500

    Adding the Main Activity

--- InventoryManager/app/src/main/java/com/example/s531373/inventorymanager/ViewActivity.java
+++ InventoryManager/app/src/main/java/com/example/s531373/inventorymanager/ViewActivity.java
@@ -10,7 +10,7 @@ public class ViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edititem);
+        setContentView(R.layout.activity_view);
         Intent intent=getIntent();
     }
 
