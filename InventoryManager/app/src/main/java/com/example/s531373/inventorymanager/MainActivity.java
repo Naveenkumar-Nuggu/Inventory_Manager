b6deb0f5cffa39ebc52b6df2eeb4f8ed457bb658
Shivani Reddy Dodla <dodlashivanireddy@gmail.com>
2018-10-05 23:04:00 -0500

    Adding Delete Item

--- InventoryManager/app/src/main/java/com/example/s531373/inventorymanager/MainActivity.java
+++ InventoryManager/app/src/main/java/com/example/s531373/inventorymanager/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.s531373.inventorymanager;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button edititem1=(Button)findViewById(R.id.button3);
+        Button edititem1=(Button)findViewById(R.id.button5);
         edititem1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,6 +39,33 @@ public class MainActivity extends AppCompatActivity {
                 openEdititemactivity3();
             }
         });
+
+
+        Button delete =(Button)findViewById(R.id.btdelete);
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+                alertDialog.setMessage("Are you sure to delete this item");
+                alertDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this,"Clicked Ok", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this,"Clicked Cancel", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alertDialog.create();
+                alertDialog.show();
+            }
+        });
+
+
+
 
     }
 
