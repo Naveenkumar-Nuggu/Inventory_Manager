12bc96dbee3274a0c07f2b0489f30748375a12d5
Girish Guntuku <S531369@nwmissouri.edu>
2018-10-26 23:00:22 -0500

    "edits"

--- InventoryManager/app/src/main/java/com/example/s531373/inventorymanager/AddItem.java
+++ InventoryManager/app/src/main/java/com/example/s531373/inventorymanager/AddItem.java
@@ -1,15 +1,64 @@
 package com.example.s531373.inventorymanager;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class AddItem extends AppCompatActivity {
+
+    private Bitmap yourSelectedImage;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_additem);
         Intent i = getIntent();
 
+        Button click = (Button) findViewById(R.id.clickimg);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+
+                startActivityForResult(i,1);
+            }
+        });
+
+
+        ImageView img = (ImageView) findViewById(R.id.imageView);
+        img.setImageBitmap(yourSelectedImage);
+
 
     }
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==1){
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String filePath = cursor.getString(columnIndex);
+            cursor.close();
+
+
+            Bitmap yourSelectedImage = BitmapFactory.decodeFile(filePath);
+        }
+
+    }
+
+
+
+
+
 }
