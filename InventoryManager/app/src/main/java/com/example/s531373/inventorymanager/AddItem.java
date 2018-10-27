7ef74974c9ffde748203df1af0dedf4cb25df808
Girish Guntuku <S531369@nwmissouri.edu>
2018-10-27 00:22:04 -0500

    "Added few functionalities in Additems page"

--- InventoryManager/app/src/main/java/com/example/s531373/inventorymanager/AddItem.java
+++ InventoryManager/app/src/main/java/com/example/s531373/inventorymanager/AddItem.java
@@ -11,6 +11,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class AddItem extends AppCompatActivity {
@@ -25,17 +26,41 @@ public class AddItem extends AppCompatActivity {
         click.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_PICK,
+                Intent img = new Intent(Intent.ACTION_PICK,
                         android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
 
-                startActivityForResult(i,1);
+                startActivityForResult(img,1);
+
+
             }
         });
 
 
-        ImageView img = (ImageView) findViewById(R.id.imageView);
-        img.setImageBitmap(yourSelectedImage);
 
+
+        Button plus1 = (Button) findViewById(R.id.plus1BTN);
+        plus1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText firstin = (EditText) findViewById(R.id.number1ET);
+                String str = firstin.getText().toString();
+                int i = Integer.parseInt(str);
+                i++;
+                firstin.setText(""+i);
+            }
+        });
+
+        Button plus2 = (Button) findViewById(R.id.plus2BTN);
+        plus2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText seconin = (EditText) findViewById(R.id.number2ET);
+                String str = seconin.getText().toString();
+                int i = Integer.parseInt(str);
+                i++;
+                seconin.setText(""+i);
+            }
+        });
 
     }
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -53,6 +78,8 @@ public class AddItem extends AppCompatActivity {
 
 
             Bitmap yourSelectedImage = BitmapFactory.decodeFile(filePath);
+            ImageView imgsrc = (ImageView) findViewById(R.id.imageView);
+            imgsrc.setImageBitmap(yourSelectedImage);
         }
 
     }
