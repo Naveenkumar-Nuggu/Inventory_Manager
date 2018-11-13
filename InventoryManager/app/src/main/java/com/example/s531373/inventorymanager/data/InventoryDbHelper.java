d16c15863bb0dc492bf20d5cccb94c905ee9afff
Chaithanya Cherukuru <S531495@nwmissouro.edu>
2018-11-13 00:26:22 -0600

    "InventoryDbHelper"

---
+++ InventoryManager/app/src/main/java/com/example/s531373/inventorymanager/data/InventoryDbHelper.java
@@ -0,0 +1,25 @@
+package com.example.s531373.inventorymanager.data;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class InventoryDbHelper extends SQLiteOpenHelper {
+
+    public InventoryDbHelper(@androidx.annotation.Nullable Context context
+            , @androidx.annotation.Nullable String name, @androidx.annotation.Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
